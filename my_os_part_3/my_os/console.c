21f46da660d4d9034eaaf0369eb3771cd4c44868
Cole Tistan <coletistan@gmail.com>
2020-12-18 20:39:09 -0500

    Update console.c

--- my_os_part_3/my_os/console.c
+++ my_os_part_3/my_os/console.c
@@ -1,6 +1,7 @@
 #include "console.h"
 unsigned int mem_index;
 
+// will move the character if it is out of scope
 void shift(){
 	mem_index = 2 * CURR_ADDR;
 	if(mem_index >= (VGA_WIDTH * VGA_HEIGHT * 2)){
@@ -13,6 +14,7 @@ void shift(){
 	}	
 }
 
+// prints on a single line.
 void terminalPrint(char* str_input){
 	unsigned int str_index = 0;
 	mem_index = 2 * CURR_ADDR;
@@ -28,6 +30,7 @@ void terminalPrint(char* str_input){
 
 }
 
+// prints on a single line and then adds a newline.
 void terminalPrintln(char* string){
 	terminalPrint(string);
 	CURR_ADDR += (VGA_WIDTH - (CURR_ADDR % VGA_WIDTH));
